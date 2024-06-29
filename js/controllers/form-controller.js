9d6e0fa8b35f14220c205d2952cf508cac1e8c3c
Vinicius Prado <viniciusprado.prado@gmail.com>
2024-06-28 22:15:25 -0300

    Projeto JS de CEP

---
+++ js/controllers/form-controller.js
@@ -0,0 +1,126 @@
+import Address from '../models/address.js';
+import * as addressService from '../services/address-service.js';
+import * as listController from './list-controller.js';
+
+function State(){
+
+    this.address = new Address();
+
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+const state = new State();
+
+export function init(){
+
+    state.btnSave = document.forms.newAddress.btnSave;
+    state.btnClear= document.forms.newAddress.btnClear;
+
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity = document.forms.newAddress.city;
+
+    state.errorCep = document.querySelector('[data-error="cep"]');
+    state.errorNumber = document.querySelector('[data-error="number"]');
+ 
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.inputNumber.addEventListener('keyup', handleInputNumberkeyup);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+    state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.inputCep.addEventListener('change', handleInputCepChange);
+
+}
+
+function handleInputNumberkeyup(event){
+    state.address.number = event.target.value;
+}
+
+async function handleInputCepChange(event){
+    const cep = event.target.value;
+
+    try{
+        const address = await addressService.findByCep(cep);
+
+        state.inputStreet.value = address.street;
+        state.inputCity.value = address.city;
+        state.address = address;
+    
+        setFormError("cep", "")
+        state.inputNumber.focus();
+    }
+    catch (e){
+        state.inputStreet.value = "";
+        state.inputCity.value = "";
+        setFormError("cep", "Informe um CEP valido");
+    }
+}
+
+function handleBtnSaveClick(event){
+
+    event.preventDefault();
+
+    const errors = addressService.getErrors(state.address);
+
+    const keys = Object.keys(errors)
+
+    if(keys.length > 0){
+       
+        keys.forEach(key =>{
+            setFormError(key, errors[key]);
+        });
+
+       // for(let i = 0; i < keys.length; i++){   
+         //   setFormError(keys[i], errors[keys[i]]);
+        //}
+    }
+
+    else {
+        listController.addCard(state.address);
+        clearForm();
+    }
+
+   
+}
+
+function handleInputNumberChange(event){
+    if(event.target.value == ""){
+        setFormError("number","Campo Requerido");
+    }
+    else{
+        setFormError("number","");
+    }
+}
+
+function handleBtnClearClick(event){
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm(){
+    state.inputCep.value = "";
+    state.inputCity.value = "";
+    state.inputNumber.value = "";
+    state.inputStreet.value = "";
+
+    setFormError("cep","");
+    setFormError("number","");
+
+    state.address = new Address();
+
+    state.inputCep.focus();
+}
+
+function setFormError(key,value){
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
+}
---
+++ js/controllers/page-controller.js
@@ -0,0 +1,15 @@
+import * as modalController from './modal-controller.js';
+
+export function init(){
+
+    const contactLink = document.querySelector(".contact-link");
+    contactLink.addEventListener('click', handleContactLinkClick);
+
+    console.log("Page controller iniciado");
+
+}
+
+function handleContactLinkClick(event){
+    event.preventDefault();
+    modalController.showModal();
+}
